707ae860a04db17d3788060514f01f471a855764
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2022-02-13 21:42:11 -0300

    arrumando align

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -56,7 +56,12 @@ export default function Dashboard() {
       <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignItems="flex-start"
+        >
           <Box p={["3", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
